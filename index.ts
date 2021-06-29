68c6af97b23f0dacf5c3d2cf722bfc2680a076f8
Codrin Cotarlan <codrin.cotarlan@softwire.com>
2021-06-29 13:32:24 +0100

    Refactored function names and return values; Refactored common array contain check code into a function

--- index.ts
+++ index.ts
@@ -2,37 +2,41 @@ import * as readline from 'readline';
 
 function printNumbers(limit: number, numbers: number[]): void {
     for (const value of Array(limit).fill(0).map((_, index) => index + 1)) {
-        printValue(value, numbers);
+        console.log(convertNumberToString(value, numbers));
     }
 }
 
-function printValue(value: number, numbers: number[]) {
+function contains(numbers: number[], number: number): boolean {
+    return numbers.findIndex((value) => value == number) != -1;
+}
+
+function convertNumberToString(value: number, numbers: number[]): String {
     let reprez: String[] = [];
 
-    if (value % 3 == 0 && numbers.findIndex((number) => number == 3) != -1) {
+    if (value % 3 == 0 && contains(numbers, 3)) {
         reprez.push("Fizz");
     }
-    if (value % 5 == 0 && numbers.findIndex((number) => number == 5) != -1) {
+    if (value % 5 == 0 && contains(numbers, 5)) {
         reprez.push("Buzz");
     }
-    if (value % 7 == 0 && numbers.findIndex((number) => number == 7) != -1) {
+    if (value % 7 == 0 && contains(numbers, 7)) {
         reprez.push("Bang");
     }
-    if (value % 11 == 0 && numbers.findIndex((number) => number == 11) != -1) {
+    if (value % 11 == 0 && contains(numbers, 11)) {
         reprez = ["Bong"];
     }
-    if (value % 13 == 0 && numbers.findIndex((number) => number == 13) != -1) {
+    if (value % 13 == 0 && contains(numbers, 13)) {
         const firstPozB = reprez.findIndex((value) => value[0] == "B");
         reprez.splice(firstPozB, 0, "Fezz");
     }
-    if (value % 17 == 0 && numbers.findIndex((number) => number == 17) != -1) {
+    if (value % 17 == 0 && contains(numbers, 17)) {
         reprez = reprez.reverse();
     }
 
     if (reprez.length == 0) {
-        console.log(value);
+        return value.toString();
     } else {
-        console.log(reprez.join(""));
+        return reprez.join("");
     }
 }
 
@@ -42,10 +46,10 @@ const reader = readline.createInterface({
 })
 reader.question("Please input the limit of the program\n", (input1) => {
     reader.question("Please specify which rules you want to apply. Examples: \n" +
-        "all\n" +
-        "none\n" +
-        "3\n" +
-        "3, 5, 13", (input2) => {
+        "  * all\n" +
+        "  * none\n" +
+        "  * 3\n" +
+        "  * 3, 5, 13\n", (input2) => {
         let numbers: number[] = [];
         if (input2 == "all") {
             numbers = [3,5,7,11,13,17]
